7ea361ced607c109ab4f57cba8b7f6c3449a45f0
shahroz <bbroya123@gmail.com>
2024-08-16 03:16:50 -0700

    b3

---
+++ seeing_the_world.py
@@ -0,0 +1,10 @@
+place = ['lahore','Badshahi_Mosque','Faisal_Mosque','Shalimar_Gardens']
+place.sort()
+
+print(f'sorted list{place}')
+
+place.reverse()
+
+print(f'reversed list{place}')
+
+print(f'original list{sorted(place)}')
