0c1f364c6ad7c45f341dcddb90120a937b5dc2d7
shahroz <bbroya123@gmail.com>
2024-08-16 04:20:01 -0700

    complete

---
+++ Intentional_Error.py
@@ -0,0 +1,7 @@
+list_1 = ['ALI','AHMED','TAHA','OSMAN','Baber']
+
+
+
+
+print(f'{list_1[5]}')
+#it will show error because i give wrong index  of list
---
+++ every_function.py
@@ -0,0 +1,53 @@
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)  
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "orange")  
+print(fruits)  
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)  
+
+
+
+
+fruits = ["apple", "banana", "cherry"]
+removed_fruit = fruits.pop()  
+print(removed_fruit)  
+print(fruits)  
+
+
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.clear()
+print(fruits)  
+
+
+
+
+digits = ["7", "3", "9","1", "2", "4"]
+index_of_digit = digits.index("banana")
+print(index_of_digit)  
+
+
+
+
+
+mix = ["apple", "1", "cherry", "1"]
+count = mix.count("1")
+print(count)  
+
+
+fruits = ["apple", "banana", "cherry"]
+fruits.sort()
+print(fruits)  
+
+
+abc = ["s", "b", "c",]
+abc.reverse()
+print(abc) 
